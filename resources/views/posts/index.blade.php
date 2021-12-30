d4ea5856cdca0a9fa75c46e310459e4b2f651e88
C1C0 <kpskola1002@gmail.com>
2021-12-30 18:29:20 +0100

    Blade - @break, @continue, @loop

--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -9,7 +9,12 @@
 @section('content')
 
     @forelse($posts as $key => $post)
-        <h3> {{ $key }}. {{ $post['title'] }}</h3>
+        {{-- Possibilty to use @break, @continue, $loop --}}
+        @if($loop->even)
+            <h3> {{ $key }}. {{ $post['title'] }}</h3>
+        @else
+            <h3 style="background-color: #ddd"> {{ $key }}. {{ $post['title'] }}</h3>
+        @endif
     @empty
         <div> No posts here</div>
     @endforelse
