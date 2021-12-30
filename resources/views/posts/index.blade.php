ecff30383b97709befb58b7dc34fc8fc9a8c6a33
C1C0 <kpskola1002@gmail.com>
2021-12-30 18:23:58 +0100

    Blade - custom directives

--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -7,7 +7,14 @@
 @extends('layouts.app')
 
 @section('content')
+
+    @notEmpty($posts)
+
     @foreach($posts as $key => $post)
         <h3> {{ $key }}. {{ $post['title'] }}</h3>
     @endforeach
+
+    @else
+        <div> No posts posted here </div>
+    @endnotEmpty
 @endsection
