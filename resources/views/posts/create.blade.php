7492be2dea9a2db1bca0e25dc3238322c0dfcf8a
C1C0 <kpskola1002@gmail.com>
2022-01-03 17:55:25 +0100

    Forms - Displaying validation errors - ShareErrorsFromSession middleware

--- resources/views/posts/create.blade.php
+++ resources/views/posts/create.blade.php
@@ -9,7 +9,24 @@
         @csrf
 
         <div><input type="text" name="title"></div>
+        @error('title')
+        <p>{{$message}}</p>
+        @enderror
+        {{-- show specific error --}}
+
         <div><textarea name="content"></textarea></div>
+
+        {{-- Show all errors --}}
+        @if($errors->any())
+            <div>
+                <ul>
+                    @foreach($errors->all() as $error)
+                        <li> {{ $error }} </li>
+                    @endforeach
+                </ul>
+            </div>
+        @endif
+
         <div><input type="submit" name="Create"></div>
     </form>
 @endsection
