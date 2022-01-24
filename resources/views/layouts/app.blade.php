d8dfe021dd5d8e250200d7e4278fe95ee19fb034
C1C0 <kpskola1002@gmail.com>
2022-01-24 18:05:02 +0100

    Assets and styling - cache assets - versioning assets - usable mainly in production

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -10,8 +10,11 @@
     <meta name="viewport"
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <link rel="stylesheet" href="{{ asset('css/app.css') }}">
-    <script src="{{ asset('js/app.js') }}" defer></script>
+
+    {{-- mix() takes care of versioning itself --}}
+    <link rel="stylesheet" href="{{ mix('css/app.css') }}">
+    <script src="{{ mix('js/app.js') }}" defer></script>
+
     <title>Laravel App - @yield('title')</title>
 </head>
 <body>
