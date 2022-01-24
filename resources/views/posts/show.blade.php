cd317050ffae1c4fcf9f0218c94cb7838c3e637a
C1C0 <kpskola1002@gmail.com>
2022-01-24 18:36:03 +0100

    Assets and styling - styling post page - clean up, using DateTime carbon library

--- resources/views/posts/show.blade.php
+++ resources/views/posts/show.blade.php
@@ -1,44 +1,23 @@
 @php
     /**
- * @var array $post
+ * @var object $post
  */
 @endphp
 
 @extends('layouts.app')
 
-@section('title', $post['title'])
+@section('title', $post->title)
 
 @section('content')
-    {{-- Mustache syntax - before outputing data, run through htmlspecialchars function --}}
-    <h1>{{ $post['title'] }}</h1>
-    <p> {{ $post['content'] }}</p>
-@endsection
+    <h1>{{ $post->title }}</h1>
+    <p> {{ $post->content }}</p>
 
-{{-- Conditional rendering --}}
-@if($post['is_new'])
-    <div>A new blog post</div>
-@elseif(!$post['is_new'])
-    <div>An old blog post</div>
-@else
-    <div>Dummy data</div>
-@endif
+    {{-- Using carbon time library --}}
+    <p>Added {{ $post->created_at->diffForHumans() }}</p>
 
-@unless($post['is_new'])
-    <div>Using unless on old post</div>
-@endunless
+    {{-- Carbon diffInMinutes --}}
+    @if(now()->diffInMinutes($post->created_at) < 5)
+        <div class="alert alert-info">New !</div>
+    @endif
 
-{{-- same as isset() in php --}}
-@isset($post['has_comments'])
-    <div>The post has some comments</div>
-@endisset
-
-{{-- same as empty() in php --}}
-@empty($post['has_comments'])
-    <div>The post doesn't have any comments</div>
-@endempty
-
-@section('footer')
-    @parent
-
-    <h4>I'm very important here as well</h4>
 @endsection
